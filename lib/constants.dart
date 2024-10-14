712fcd8ce9636c06f89d2aabba3f4a63f2d486f7
HayHay <dev@hayhay.dev>
2024-10-13 21:33:03 -0400

    Add more consts

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,12 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 const String baseUrl = 'https://api.themoviedb.org/3';
 const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
+const String smallImageBaseUrl = 'https://image.tmdb.org/t/p/w400';
+
+final String apiKey = dotenv.env['BEARER_TOKEN']!;
+final headers = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'Authorization': 'Bearer $apiKey',
+};
