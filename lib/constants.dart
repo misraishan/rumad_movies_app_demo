51a7c92f9f1d7f6eb04dc83e710e64aae2ce36ff
HayHay <dev@hayhay.dev>
2024-10-14 13:22:10 -0400

    Comments

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,12 +1,22 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+// This file contains some base variables that's used throughout the app
+// In particular, it has to do with the API we're using, and the base
+// URLs for api and image/cdn domain, along with the headers needed for
+// the API requests
+
 const String baseUrl = 'https://api.themoviedb.org/3';
 const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
 const String smallImageBaseUrl = 'https://image.tmdb.org/t/p/w400';
 
-final String apiKey = dotenv.env['BEARER_TOKEN']!;
+// In normal cases, you wouldn't want to expose an API key
+// Also, we're using a hardcoded bearer token here, which is not recommended
+// This would only authenticate for YOUR user, and not for all users
+// Actual authentication should be done on the server side, but is
+// out of scope for this tutorial.
+final String bearerToken = dotenv.env['BEARER_TOKEN']!;
 final headers = {
   'Content-Type': 'application/json',
   'Accept': 'application/json',
-  'Authorization': 'Bearer $apiKey',
+  'Authorization': 'Bearer $bearerToken',
 };
